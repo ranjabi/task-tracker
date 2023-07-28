ca5003572ec9eec6900545a678507b8a90330e63
ranjabi <fikriranjabi02@gmail.com>
2023-07-28 11:20:15 +0700

    feat: history api

--- src/app/api/stopwatch/route.ts
+++ src/app/api/stopwatch/route.ts
@@ -1,6 +1,67 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from 'next/server';
+import { PrismaClient } from '@prisma/client';
 
-export async function POST(request: Request) {
-  const body = await request.json()
-  return NextResponse.json({ message: body })
+type Timestamp = {
+  id: number;
+  startTime: Number;
+  endTime: Number;
+  secondsElapsed: number;
+};
+
+export async function GET(req: Request) {
+  const prisma = new PrismaClient();
+  const allTask = await prisma.task.findMany({
+    include: {
+      sessions: true
+    },
+    where: {
+      user: {
+        username: 'Ranjabi'
+      },
+      sessions: {
+        some: {}
+      }
+    }
+  })
+
+  return NextResponse.json({ data: allTask });
+}
+
+export async function POST(req: Request) {
+  const body: Timestamp[] = await req.json();
+
+  const prisma = new PrismaClient();
+  const taskId = Math.floor(Math.random() * 1000) + 1
+  const newTask = await prisma.task.create({
+    data: {
+      name: `Task ${taskId}`,
+      user: {
+        connect: { username: 'Ranjabi' },
+      },
+    },
+  });
+
+  body.forEach(async ({startTime, endTime, secondsElapsed}) => {
+    await prisma.session.create({
+      data: {
+        startTime: new Date(Number(startTime)),
+        endTime: new Date(Number(endTime)),
+        duration: secondsElapsed,
+        task: {
+          connect: { id: newTask.id }
+        },
+      },
+    });
+  });
+
+  const task = await prisma.task.findFirst({
+    include: {
+      sessions: true
+    },
+    where: {
+      name: `Task ${taskId}`
+    }
+  })
+
+  return NextResponse.json({ data: task });
 }
