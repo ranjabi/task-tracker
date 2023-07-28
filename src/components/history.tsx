e139900fe83dd135d6e72fde3df653626d8fac61
ranjabi <fikriranjabi02@gmail.com>
2023-07-28 17:00:35 +0700

    feat: change day and remove task

--- src/components/history.tsx
+++ src/components/history.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { Task, Session } from '@prisma/client';
+import clsx from 'clsx';
 import { useEffect, useState } from 'react';
 
 type TaskWithSession = Task & {
@@ -21,51 +22,85 @@ export default function History() {
     fetchData();
   }, []);
 
+  const [date, setDate] = useState(new Date());
+
+  function incrementDay() {
+    setDate((prev) => new Date(prev.getTime() + 86400000));
+  }
+
+  function decrementDay() {
+    setDate((prev) => new Date(prev.getTime() - 86400000));
+  }
+
+  function isDisabledNextDay() {
+    const today = new Date();
+    const todayDate = formatDateToDayMonthYear(today);
+    const dateDate = formatDateToDayMonthYear(date);
+
+    return todayDate === dateDate;
+  }
+
   return (
     <div className="px-6 w-full mt-10">
       <div className="font-semibold text-2xl">History</div>
-      <div className="mt-4">
+      <div className='flex justify-between items-center mt-4'>
+        <button className='btn-solid-primary ' onClick={decrementDay}>{'<'}</button>
+        <p className='text-xl'>{formatDateToDayMonthYear(date)}</p>
+        <button className={clsx('btn-solid-primary', isDisabledNextDay() ? 'bg-blue-100 hover:bg-blue-100' : '')} onClick={incrementDay} disabled={isDisabledNextDay() ? true : false}>{'>'}</button>
+      </div>
+      <div className="">
         {data.map((task) => {
           const taskName = task.name;
           const taskDate = formatDateToDayMonthYear(
             new Date(task.sessions[0].startTime)
           );
-          return (
-            <div key={task.id} className="mb-8">
-              <div className="flex justify-between">
-                <p>{taskName}</p>
-                <p>{taskDate}</p>
+
+          if (taskDate === formatDateToDayMonthYear(date)) {
+            return (
+              <div key={task.id} className="mt-8">
+                <div className="flex justify-between items-center">
+
+                  <p>{taskName}</p>
+                  <button className={clsx('btn-outline-primary', 'text-xs px-2 py-0.5 rounded-md')} onClick={() => {
+                    fetch(`/api/stopwatch/${task.id}`, {
+                      method: 'DELETE',
+                    })
+                    setData(data.filter((item) => item.id !== task.id))
+                  }}>X</button>
+                </div>
+                <table className="w-full mt-1">
+                  <thead className="">
+                    <tr className="border bg-blue-100">
+                      <th className="py-2 text-center font-semibold">
+                        Start Time
+                      </th>
+                      <th className="py-2 text-center font-semibold">End Time</th>
+                      <th className="py-2 text-center font-semibold">Duration</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {task.sessions.map((session) => {
+                      return (
+                        <tr key={session.id} className="border">
+                          <td className="py-2 text-center">
+                            {formatDateToHHMMSS(new Date(session.startTime))}
+                          </td>
+                          <td className="py-2 text-center">
+                            {formatDateToHHMMSS(new Date(session.endTime))}
+                          </td>
+                          <td className="py-2 text-center">
+                            {session.duration} detik
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
               </div>
-              <table className="w-full mt-1">
-                <thead className="">
-                  <tr className="border bg-blue-100">
-                    <th className="py-2 text-center font-semibold">
-                      Start Time
-                    </th>
-                    <th className="py-2 text-center font-semibold">End Time</th>
-                    <th className="py-2 text-center font-semibold">Duration</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {task.sessions.map((session) => {
-                    return (
-                      <tr key={session.id} className="border">
-                        <td className="py-2 text-center">
-                          {formatDateToHHMMSS(new Date(session.startTime))}
-                        </td>
-                        <td className="py-2 text-center">
-                          {formatDateToHHMMSS(new Date(session.endTime))}
-                        </td>
-                        <td className="py-2 text-center">
-                          {session.duration} detik
-                        </td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </table>
-            </div>
-          );
+            );
+          }
+
+          
         })}
       </div>
     </div>
