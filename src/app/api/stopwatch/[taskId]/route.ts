406b22d9d41477436fad48d09499c41a4434efbd
ranjabi <fikriranjabi02@gmail.com>
2023-08-03 15:34:37 +0700

    feat: get single task id

--- src/app/api/stopwatch/[taskId]/route.ts
+++ src/app/api/stopwatch/[taskId]/route.ts
@@ -1,11 +1,31 @@
 import { PrismaClient } from '@prisma/client';
 import { NextResponse } from 'next/server';
 
+export async function GET(
+  req: Request,
+  { params }: { params: { taskId: string } }
+) {
+  const taskId = params.taskId;
+  const prisma = new PrismaClient();
+  const task = await prisma.task.findFirst({
+    include: {
+      sessions: true,
+    },
+    where: {
+      id: taskId,
+      sessions: {
+        some: {},
+      },
+    },
+  });
+  return NextResponse.json({ data: task });
+}
+
 export async function DELETE(
   req: Request,
   { params }: { params: { taskId: string } }
 ) {
-  const taskId = params.taskId
+  const taskId = params.taskId;
   const prisma = new PrismaClient();
   const deleteSessions = prisma.session.deleteMany({
     where: {
